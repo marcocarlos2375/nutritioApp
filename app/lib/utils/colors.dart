2137a3fe38f96b396411dc01c02fd40ec8113046
marcocarlos2375 <idrisscarlosid1@gmail.com>
2023-06-07 08:46:42 +0200

    third

---
+++ app/lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF0D47A1); // Blue color
+  static const secondaryColor = Color(0xFF64DD17); // Green color
+}
